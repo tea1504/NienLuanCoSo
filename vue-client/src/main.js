5641b4375e948b85643a99711c885b3d2b825ad7
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-04-28 14:52:38 +0700

    Test Project

--- vue-client/src/main.js
+++ vue-client/src/main.js
@@ -1,5 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+var $ = require( "jquery" );
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+import VueSweetalert2 from 'vue-sweetalert2';
+
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+Vue.use(VueSweetalert2);
 
 new Vue({
   el: '#app',
